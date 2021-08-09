5ff27015c13143ab3ca957b3f0bf435071d31774
Sunny Mei <sunmeiappprep@gmail.com>
2021-08-09 12:19:42 -0400

    Mov Draw

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  +  // Lodash, now imported by this script
+  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
+const MovingObject = require("./moving_object.js");
 
+window.MovingObject = MovingObject;
+
+const mo = new MovingObject({
+  pos: [30, 30],
+  vel: [10, 10],
+  radius: 5,
+  color: "#00FF00"
+});
+console.log(mo)
+
+console.log("Webpack is working")
 document.body.appendChild(component());
